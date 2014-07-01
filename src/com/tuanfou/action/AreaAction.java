845ec755c5381a6c154c00561e2be44b46c17db3
lnhust <1692185741@qq.com>
2014-07-01 09:29:58 +0800

    ln：AreaAction

--- src/com/tuanfou/action/AreaAction.java
+++ src/com/tuanfou/action/AreaAction.java
@@ -1,5 +1,46 @@
 package com.tuanfou.action;
 
-public class AreaAction {
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.google.gson.Gson;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.tuanfou.dto.AreaInfo;
+import com.tuanfou.service.AreaService;
+
+public class AreaAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private HttpServletRequest req;
+	private HttpServletResponse response;
+	private static AreaService areaService = new AreaService();
+	
+	public String loadAreas(){
+		req = (HttpServletRequest) ActionContext.getContext().get("request");
+		response = ServletActionContext.getResponse();
+		int cityId = Integer.parseInt(req.getParameter("cityId"));
+		try{
+			List<AreaInfo> areaInfoList = areaService.getAreaInfoList(cityId);
+			response.setCharacterEncoding("utf-8");
+			PrintWriter out = response.getWriter();
+			Gson gson = new Gson();
+			String res = gson.toJson(areaInfoList);
+			out.print(res);
+			return null;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return "LoadAreaSuccess";
+		}
+	}
 
 }
