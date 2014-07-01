14bea74e9c97ca4a786896f3e56dfaf26a5a934a
yogiman <xuyuji9000@163.com>
2014-07-01 09:39:24 +0800

    get back,test down

--- src/com/tuanfou/dao/GroupFilmDao.java
+++ src/com/tuanfou/dao/GroupFilmDao.java
@@ -118,7 +118,7 @@ public class GroupFilmDao {
 					groupFilmDetailedInfo.setShowDate(groupFilm.getStartDate());
 					groupFilmDetailedInfo.setorderNum(getOderNum(groupFilm.getId()));
 					groupFilmDetailedInfo.setSummary(groupFilm.getFilm().getDescription());
-//					TODO 	SET TAGS start to do
+//					TODO 	SET TAGS
 //					groupFilmDetailedInfo.setTags(groupFilm.getFilm().getTags());
 //					TODO	SET TIME RANGE IN THE DAYLIGHT
 					
