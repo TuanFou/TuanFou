cad8c239c781c8a8aab320ba005dc8e408aa80d0
yogiman <xuyuji9000@163.com>
2014-07-01 11:21:48 +0800

    Signed-off-by: yogiman <xuyuji9000@163.com> 一个注释

--- src/com/tuanfou/test/GroupFilmDaoTest.java
+++ src/com/tuanfou/test/GroupFilmDaoTest.java
@@ -20,7 +20,7 @@ public class GroupFilmDaoTest {
 	 */
 	public static void main(String[] args) {
 //		TODO some test
-		changeDateFormatTest();
+		//changeDateFormatTest();
 	}
 	public static void groupfilmBriefInfoTest(){
 //		private int id;
