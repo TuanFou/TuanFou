ffc1bfcec232c0622ee0309a501037357e41367a
yogiman <xuyuji9000@163.com>
2014-07-03 11:19:21 +0800

    yogiman: get complaints of the target user
    Signed-off-by: yogiman <xuyuji9000@163.com>

---
+++ src/com/tuanfou/test/ComplaintDaoTest.java
@@ -0,0 +1,32 @@
+package com.tuanfou.test;
+
+import java.util.List;
+
+import com.tuanfou.dao.ComplaintDao;
+import com.tuanfou.dto.ComplaintInfo;
+import com.tuanfou.pojo.Complaint;
+import com.tuanfou.utils.GsonTestTemplate;
+
+public class ComplaintDaoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		getUserComplaintsTest();
+	}
+	
+	/**
+	 * test getUserComplaints()
+	 * @author yogiman
+	 */
+	public static void getUserComplaintsTest(){
+		ComplaintDao complaintDao = new ComplaintDao();
+		List<ComplaintInfo> complaints = complaintDao.getUserComplaints(319010023, 0, 5);
+//		Complaint complaint = complaints.get(0);
+//		System.out.println(complaint.getUser().getId());
+		GsonTestTemplate.testListToString(complaints);
+	}
+
+}
